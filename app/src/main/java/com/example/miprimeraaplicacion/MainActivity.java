8e2999af791c9f6c7771dcbdc08070e756f9f284
valen500 <brianalvarado078@gmail.com>
2025-01-31 13:29:52 -0600

    opciones suma,resta y multiplicacion agregadas

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.miprimeraaplicacion;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView tempVal;
+    RadioGroup rgo;
+
+    RadioButton opt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +28,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 tempVal = findViewById(R.id.txtNum1);
                 double num1 = Double.parseDouble(tempVal.getText().toString());
+
                 tempVal = findViewById(R.id.txtNum2);
                 double num2 = Double.parseDouble(tempVal.getText().toString());
-                double respuesta = num1 + num2;
+
+                double respuesta = 0.0;
+
+                opt = findViewById(R.id.optsuma);
+                if (opt.isChecked()) {
+                    respuesta = num1 + num2;
+                }
+                opt = findViewById(R.id.optresta);
+                if (opt.isChecked()) {
+                    respuesta = num1 - num2;
+                }
+                opt = findViewById(R.id.optmultiplicacion);
+                if (opt.isChecked()) {
+                    respuesta = num1 * num2;
+                }
+                opt = findViewById(R.id.optdivision);
+                if (opt.isChecked()) {
+                    respuesta = num1 / num2;
+                }
                 tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
+                tempVal.setText("Respuesta: " + respuesta);
+
             }
         });
     }
