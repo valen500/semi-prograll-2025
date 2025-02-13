685cc4777311b0803bf1890e7f54044134098713
valen500 <brianalvarado078@gmail.com>
2025-02-13 15:56:36 -0600

    Funcionamiento del sensor de luz

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -1,16 +1,70 @@
 package com.example.miprimeraaplicacion;
 
 import android.os.Bundle;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView tempVal;
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        sensorLuz();
+    }
+    @Override
+    protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+    private void iniciar(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+    private void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+    private void sensorLuz(){
+        tempVal = findViewById(R.id.lblSensorLuz);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if( sensor==null ){
+            tempVal.setText("Tu dispositivo, NO tiene el senor de LUZ");
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                double valor = event.values[0];
+                tempVal.setText("Cantidad de Luz: "+ valor);
+                if(valor<=20){
+                    getWindow().getDecorView().setBackgroundColor(Color.GRAY);
+                }else if(valor<=50){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                }else{
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
     }
 }
