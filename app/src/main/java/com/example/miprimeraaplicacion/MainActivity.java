51e41095c0a947a28380ddbd8cf5b2a5c7d47a4e
valen500 <brianalvarado078@gmail.com>
2025-02-13 16:04:34 -0600

    creacion y Funcionamiento de la rama sensor de proximidad

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -42,24 +42,24 @@ public class MainActivity extends AppCompatActivity {
         sensorManager.unregisterListener(sensorEventListener);
     }
     private void sensorLuz(){
-        tempVal = findViewById(R.id.lblSensorLuz);
+        tempVal = findViewById(R.id.lblSensorProximidad);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         if( sensor==null ){
-            tempVal.setText("Tu dispositivo, NO tiene el senor de LUZ");
+            tempVal.setText("Tu dispositivo, NO tiene el sensor de proximidad");
             finish();
         }
         sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
                 double valor = event.values[0];
-                tempVal.setText("Cantidad de Luz: "+ valor);
-                if(valor<=20){
+                tempVal.setText("Proximidad: "+ valor);
+                if(valor<=4){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+                }else if(valor<=8){
                     getWindow().getDecorView().setBackgroundColor(Color.GRAY);
-                }else if(valor<=50){
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
                 }else{
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
                 }
             }
             @Override
