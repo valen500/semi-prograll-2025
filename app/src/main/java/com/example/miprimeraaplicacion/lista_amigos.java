52a6d06c8118b45ddbdf34c714bcb217f5dd637e
valen500 <brianalvarado078@gmail.com>
2025-05-18 16:54:30 -0600

    final de la agenda amigos y eliminacion

--- app/src/main/java/com/example/miprimeraaplicacion/lista_amigos.java
+++ app/src/main/java/com/example/miprimeraaplicacion/lista_amigos.java
@@ -1,6 +1,5 @@
 package com.example.miprimeraaplicacion;
 
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -97,6 +96,22 @@ public class lista_amigos extends Activity {
             confirmacion.setMessage(nombre);
             confirmacion.setPositiveButton("Si", (dialog, which) -> {
                 try {
+                    di = new DetectarInternet(this);
+                    if(di.hayConexionInternet()){//online
+                        JSONObject datosAmigos = new JSONObject();
+                        String _id = jsonArray.getJSONObject(posicion).getJSONObject("value").getString("_id");
+                        String _rev = jsonArray.getJSONObject(posicion).getJSONObject("value").getString("_rev");
+                        String url = utilidades.url_mto + "/" + _id + "?rev=" + _rev;
+                        enviarDatosServidor objEnviarDatosServidor = new enviarDatosServidor(this);
+                        String respuesta = objEnviarDatosServidor.execute(datosAmigos.toString(), "DELETE", url).get();
+                        JSONObject respuestaJSON = new JSONObject(respuesta);
+                        if(respuestaJSON.getBoolean("ok")) {
+                            obtenerDatosAmigos();
+                            mostrarMsg("Registro eliminado con exito");
+                        }else{
+                            mostrarMsg("Error: " + respuesta);
+                        }
+                    }
                     String respuesta = db.administrar_amigos("eliminar", new String[]{jsonArray.getJSONObject(posicion).getJSONObject("value").getString("idAmigo")});
                     if(respuesta.equals("ok")) {
                         obtenerDatosAmigos();
