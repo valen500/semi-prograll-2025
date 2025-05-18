690894af42133f01e3cc2138079c949266cea9b6
valen500 <brianalvarado078@gmail.com>
2025-05-18 17:29:34 -0600

    Creacion de la pantalla de registro de usuario

---
+++ app/src/main/java/com/example/miprimeraaplicacion/registro.java
@@ -0,0 +1,13 @@
+package com.example.miprimeraaplicacion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class registro extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
