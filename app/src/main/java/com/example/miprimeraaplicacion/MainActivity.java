8182d8f9070e6933a7538ad025318daa42ed1c5a
valen500 <brianalvarado078@gmail.com>
2025-05-18 16:03:34 -0600

    Correccion de la accion al guardar las modificaciones y el ID, y se agrego eliminar registros

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.miprimeraaplicacion;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -8,10 +12,16 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
     Button btn;
     TextView tempVal;
     DB db;
+    String accion = "nuevo", idAmigo = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +29,46 @@ public class MainActivity extends AppCompatActivity {
 
         db = new DB(this);
         btn = findViewById(R.id.btnGuardarAmigo);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                guardarAmigo();
+        btn.setOnClickListener(view->guardarAmigo());
+
+        fab = findViewById(R.id.fabListaAmigos);
+        fab.setOnClickListener(view->abrirVentana());
+
+        mostrarDatos();
+    }
+    private void mostrarDatos(){
+        try {
+            Bundle parametros = getIntent().getExtras();
+            accion = parametros.getString("accion");
+            if (accion.equals("modificar")) {
+                JSONObject datos = new JSONObject(parametros.getString("amigos"));
+                idAmigo = datos.getString("idAmigo");
+
+                tempVal = findViewById(R.id.txtNombre);
+                tempVal.setText(datos.getString("nombre"));
+
+                tempVal = findViewById(R.id.txtDireccion);
+                tempVal.setText(datos.getString("direccion"));
+
+                tempVal = findViewById(R.id.txtTelefono);
+                tempVal.setText(datos.getString("telefono"));
+
+                tempVal = findViewById(R.id.txtEmail);
+                tempVal.setText(datos.getString("email"));
+
+                tempVal = findViewById(R.id.txtDui);
+                tempVal.setText(datos.getString("dui"));
             }
-        });
+        }catch (Exception e){
+            mostrarMsg("Error: "+e.getMessage());
+        }
+    }
+    private void mostrarMsg(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+    private void abrirVentana(){
+        Intent intent = new Intent(this, lista_amigos.class);
+        startActivity(intent);
     }
     private void guardarAmigo() {
         tempVal = findViewById(R.id.txtNombre);
@@ -41,9 +85,9 @@ public class MainActivity extends AppCompatActivity {
         tempVal = findViewById(R.id.txtDui);
         String dui = tempVal.getText().toString();
 
-        String[] datos = {"", nombre, direccion, telefono, email, dui, ""};
-        db.administrar_amigos("agregar", datos);
+        String[] datos = {idAmigo, nombre, direccion, telefono, email, dui, ""};
+        db.administrar_amigos(accion, datos);
         Toast.makeText(getApplicationContext(), "Registro guardado con exito.", Toast.LENGTH_LONG).show();
+        abrirVentana();
     }
-
 }
--- app/src/main/java/com/example/miprimeraaplicacion/lista_amigos.java
+++ app/src/main/java/com/example/miprimeraaplicacion/lista_amigos.java
@@ -2,16 +2,23 @@ package com.example.miprimeraaplicacion;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -35,11 +42,13 @@ public class lista_amigos extends Activity {
     JSONObject jsonObject;
     amigos misAmigos;
     FloatingActionButton fab;
+    int posicion = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_amigos);
 
+        parametros.putString("accion", "nuevo");
         db = new DB(this);
 
         fab = findViewById(R.id.fabAgregarAmigo);
@@ -47,8 +56,67 @@ public class lista_amigos extends Activity {
         obtenerDatosAmigos();
         buscarAmigos();
     }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mimenu, menu);
+        try {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            posicion = info.position;
+            menu.setHeaderTitle(jsonArray.getJSONObject(posicion).getString("nombre"));
+        } catch (Exception e) {
+            mostrarMsg("Error: " + e.getMessage());
+        }
+    }
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        try{
+            if( item.getItemId()==R.id.mnxNuevo){
+                abriVentana();
+            }else if( item.getItemId()==R.id.mnxModificar){
+                parametros.putString("accion", "modificar");
+                parametros.putString("amigos", jsonArray.getJSONObject(posicion).toString());
+                abriVentana();
+            } else if (item.getItemId()==R.id.mnxEliminar) {
+                eliminarAmigo();
+            }
+            return true;
+        }catch (Exception e){
+            mostrarMsg("Error: " + e.getMessage());
+            return super.onContextItemSelected(item);
+        }
+    }
+    private void eliminarAmigo(){
+        try{
+            String nombre = jsonArray.getJSONObject(posicion).getString("nombre");
+            AlertDialog.Builder confirmacion = new AlertDialog.Builder(this);
+            confirmacion.setTitle("Esta seguro de eliminar a: ");
+            confirmacion.setMessage(nombre);
+            confirmacion.setPositiveButton("Si", (dialog, which) -> {
+                try {
+                    String respuesta = db.administrar_amigos("eliminar", new String[]{jsonArray.getJSONObject(posicion).getString("idAmigo")});
+                    if(respuesta.equals("ok")) {
+                        obtenerDatosAmigos();
+                        mostrarMsg("Registro eliminado con exito");
+                    }else{
+                        mostrarMsg("Error: " + respuesta);
+                    }
+                }catch (Exception e){
+                    mostrarMsg("Error: " + e.getMessage());
+                }
+            });
+            confirmacion.setNegativeButton("No", (dialog, which) -> {
+                dialog.dismiss();
+            });
+            confirmacion.create().show();
+        }catch (Exception e){
+            mostrarMsg("Error: " + e.getMessage());
+        }
+    }
     private void abriVentana(){
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtras(parametros);
         startActivity(intent);
     }
     private void obtenerDatosAmigos(){
