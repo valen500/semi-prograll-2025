69f57089bd68a8a8810f7caa04ed47fbc4b242ed
valen500 <brianalvarado078@gmail.com>
2025-05-18 16:43:44 -0600

    creacion de la rama amigos con CouchDB y la obtencion de datos del servidor

--- app/src/main/java/com/example/miprimeraaplicacion/lista_amigos.java
+++ app/src/main/java/com/example/miprimeraaplicacion/lista_amigos.java
@@ -43,6 +43,8 @@ public class lista_amigos extends Activity {
     amigos misAmigos;
     FloatingActionButton fab;
     int posicion = 0;
+    obtenerDatosServidor datosServidor;
+    DetectarInternet di;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +55,7 @@ public class lista_amigos extends Activity {
 
         fab = findViewById(R.id.fabAgregarAmigo);
         fab.setOnClickListener(view -> abriVentana());
-        obtenerDatosAmigos();
+        listarDatos();
         buscarAmigos();
     }
     @Override
@@ -119,6 +121,22 @@ public class lista_amigos extends Activity {
         intent.putExtras(parametros);
         startActivity(intent);
     }
+    private void listarDatos(){
+        try{
+            di = new DetectarInternet(this);
+            if(di.hayConexionInternet()){//online
+                datosServidor = new obtenerDatosServidor();
+                String respuesta = datosServidor.execute().get();
+                jsonObject = new JSONObject(respuesta);
+                jsonArray = jsonObject.getJSONArray("rows");
+                mostrarDatosAmigos();
+            }else{//offline
+                obtenerDatosAmigos();
+            }
+        }catch (Exception e){
+            mostrarMsg("Error: " + e.getMessage());
+        }
+    }
     private void obtenerDatosAmigos(){
         try{
             cAmigos = db.lista_amigos();
@@ -152,7 +170,7 @@ public class lista_amigos extends Activity {
                 alAmigosCopia.clear();
 
                 for (int i=0; i<jsonArray.length(); i++){
-                    jsonObject = jsonArray.getJSONObject(i);
+                    jsonObject = jsonArray.getJSONObject(i).getJSONObject("value");
                     misAmigos = new amigos(
                             jsonObject.getString("idAmigo"),
                             jsonObject.getString("nombre"),
---
+++ app/src/main/java/com/example/miprimeraaplicacion/utilidades.java
@@ -0,0 +1,14 @@
+package com.example.miprimeraaplicacion;
+
+import java.util.Base64;
+
+public class utilidades {
+    static String url_consulta = "http://192.168.13.8:5984/agenda/_design/agenda/_view/agenda";
+    static String url_mto = "http://192.168.13.8:5984/agenda";
+    static String user = "estudiante";
+    static String passwd = "estudiante";
+    static String credencialesCodificadas = Base64.getEncoder().encodeToString((user + ":" + passwd).getBytes());
+    public String generarUnicoId(){
+        return java.util.UUID.randomUUID().toString();
+    }
+}
