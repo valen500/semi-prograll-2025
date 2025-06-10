af154363fb5ed2f0fd5b54fbdf0604277528f7f8
valen500 <brianalvarado078@gmail.com>
2025-06-10 17:00:51 -0600

    continuacion  de mensajeria y notificaciones push

--- app/src/main/java/com/example/miprimeraaplicacion/chats.java
+++ app/src/main/java/com/example/miprimeraaplicacion/chats.java
@@ -1,16 +1,23 @@
 package com.example.miprimeraaplicacion;
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
@@ -32,45 +39,72 @@ import java.net.URL;
 
 public class chats extends Activity {
     ImageView img;
-    TextView tempVal;
-    Button btn;
+    TextView tempVal, txtMsg;
+    ImageButton btn;
     String to="", from="", user="", msg="", urlFoto="", urlCompletaFotoFirestore="";
     DatabaseReference databaseReference;
     private chatsArrayAdapter chatArrayAdapter;
     ListView ltsChats;
     @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter(MyFirebaseMessagingService.DISPLAY_MESSAGE_ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(notificacionPush, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(notificacionPush);
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chats);
 
-        img = findViewById(R.id.imgAtras);
-        img.setOnClickListener(view -> {
-            abrirVentana();
-        });
-        tempVal = findViewById(R.id.lblToChats);
-        Bundle parametros = getIntent().getExtras();
-        if (parametros != null && parametros.getString("to") != null && parametros.getString("to") != "") {
-            to = parametros.getString("to");
-            from = parametros.getString("from");
-            user = parametros.getString("nombre");
-            urlFoto = parametros.getString("urlFoto");
-            urlCompletaFotoFirestore = parametros.getString("urlCompletaFotoFirestore");
-            tempVal.setText(user);
-        }
-        tempVal = findViewById(R.id.txtMsgChats);
-        mostrarFoto();
-        enviarMsg();
-        ltsChats = findViewById(R.id.ltsChats);
+        try {
+            img = findViewById(R.id.imgAtras);
+            img.setOnClickListener(view -> {
+                abrirVentana();
+            });
+            txtMsg = findViewById(R.id.txtMsgChats);
+            txtMsg.setOnKeyListener((v, keyCode, event)->{
+                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    try {
+                        guardarMsgFirebase(txtMsg.getText().toString());
+                        sendChatMessage(false, txtMsg.getText().toString());
+                    }catch (Exception e){
+                        mostrarMsg(e.getMessage());
+                    }
+                }
+                return false;
+            });
+            tempVal = findViewById(R.id.lblToChats);
+            Bundle parametros = getIntent().getExtras();
+            if (parametros != null && parametros.getString("to") != null && parametros.getString("to") != "") {
+                to = parametros.getString("to");
+                from = parametros.getString("from");
+                user = parametros.getString("nombre");
+                urlFoto = parametros.getString("urlFoto");
+                urlCompletaFotoFirestore = parametros.getString("urlCompletaFotoFirestore");
+                tempVal.setText(user);
+            }
+            mostrarFoto();
+            enviarMsg();
+            ltsChats = findViewById(R.id.ltsChats);
 
-        chatArrayAdapter = new chatsArrayAdapter(getApplicationContext(), R.layout.msgizquierda);
-        ltsChats.setAdapter(chatArrayAdapter);
-        historialMsg();
+            chatArrayAdapter = new chatsArrayAdapter(getApplicationContext(), R.layout.msgizquierda);
+            ltsChats.setAdapter(chatArrayAdapter);
+            historialMsg();
+        }catch (Exception e){
+            mostrarMsg("Error al cargar la ventana de chats: "+ e.getMessage());
+        }
     }
     private void enviarMsg(){
         btn = findViewById(R.id.btnEnviarMsg);
         btn.setOnClickListener(v->{
-            guardarMsgFirebase(tempVal.getText().toString());
-            sendChatMessage(false, tempVal.getText().toString());
+            guardarMsgFirebase(txtMsg.getText().toString());
+            sendChatMessage(false, txtMsg.getText().toString());
         });
     }
     private void guardarMsgFirebase(String msg){
@@ -99,14 +133,13 @@ public class chats extends Activity {
             String key = databaseReference.push().getKey();
             databaseReference.child(key).setValue(chatsMsg);
         }catch (Exception e){
-
             mostrarMsg("Error al guardar msg en firebasemmmmm: "+ e.getMessage());
         }
     }
     private void sendChatMessage(Boolean posicion, String msg){
         try{
             chatArrayAdapter.add(new chatMessage(posicion, msg));
-            tempVal.setText("");
+            txtMsg.setText("");
         }catch (Exception e){
             mostrarMsg("Error al posicional el msg: "+ e.getMessage());
         }
@@ -147,64 +180,85 @@ public class chats extends Activity {
             }
         });
     }
-    private class enviarDatos extends AsyncTask<String, String, String>{
-        HttpURLConnection httpURLConnection;
+    private class enviarDatos extends AsyncTask<String,String,String> {
+        HttpURLConnection urlConnection;
         @Override
-        protected String doInBackground(String... parametros) {
+        protected String doInBackground(String... params) {
             StringBuilder result = new StringBuilder();
 
-            String jsonResponse = null;
-            String jsonDATA = parametros[0];
+            String JsonResponse = null;
+            String JsonDATA = params[0];
             BufferedReader reader = null;
 
-            try{
+            try {
+                //conexion al servidor...
                 URL url = new URL("https://fcm.googleapis.com/fcm/send");
-                httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setDoInput(true);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
 
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setRequestProperty("Content-Type", "application/json");
-                httpURLConnection.setRequestProperty("Accept", "application/json");
-                httpURLConnection.setRequestProperty("Authorization","key=BI8BanTAjfy-2d28jD3K4x4fhXV7qVuo8I-cWQxb0q5W-35JLEeO1nV6UcaPAN8XdsrMsSJffRU_6lqFhBlrxx0");
-                //establecer los encabezados y los dfatos
-                Writer writer = new BufferedWriter(new OutputStreamWriter(httpURLConnection.getOutputStream(),"UTF-8"));
-                writer.write(jsonDATA);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Accept", "application/json");
+                urlConnection.setRequestProperty("Authorization", "key=BI8BanTAjfy-2d28jD3K4x4fhXV7qVuo8I-cWQxb0q5W-35JLEeO1nV6UcaPAN8XdsrMsSJffRU_6lqFhBlrxx0");
+
+                //set headers and method
+                Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8"));
+                writer.write(JsonDATA);
                 writer.close();
-                //obtener datosen formato json
-                InputStream inputStream = httpURLConnection.getInputStream();
-                if(inputStream==null) return null;
+
+                // json data
+                InputStream inputStream = urlConnection.getInputStream();
+
+                if (inputStream == null) {
+                    return null;
+                }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
-                //procesar la respuesta
+
                 String inputLine;
                 StringBuffer buffer = new StringBuffer();
-                while((inputLine = reader.readLine()) != null){
-                    buffer.append(inputLine);
+                while ((inputLine = reader.readLine()) != null) {
+                    buffer.append(inputLine + "\n");
                 }
-                if(buffer.length()==0) return null;
-                jsonResponse = buffer.toString();
-                return jsonResponse;
-            }catch (Exception e){
-                mostrarMsg("Error al enviar al server de GCM: "+ e.getMessage());
-                e.printStackTrace();
-                Log.d("URI: ", "Error al enviar la notificacion: "+ e.getMessage());
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                JsonResponse = buffer.toString();
+                return JsonResponse;
+
+            }catch (Exception ex){
+                ex.printStackTrace();
+                Log.d("URI: ", "Error enviando notificacion: "+ ex.getMessage());
             }
             return null;
         }
-
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            try{
-                if(s!=null){
+            try {
+                if( s!=null ) {
                     JSONObject jsonObject = new JSONObject(s);
-                    if( jsonObject.getInt("success")<=0 ){
-                        mostrarMsg("Error "+ s);
+                    Log.d("URI: ", "Error enviando notificacion: "+ s);
+                    if (jsonObject.getInt("success") <= 0) {
+                        mostrarMsg("Error al enviar mensaje");
                     }
                 }
-            }catch (Exception e){
-                mostrarMsg("Error al enviar ls notificacion: "+ e.getMessage());
+            }catch(Exception ex){
+                mostrarMsg("Error al enviar a la red: "+ ex.getMessage());
             }
         }
     }
-}
+    private BroadcastReceiver notificacionPush = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            WakeLocker.acquire(getApplicationContext());
+
+            msg = intent.getStringExtra("msg");
+            to = intent.getStringExtra("from");
+            from = intent.getStringExtra("to");
+
+            sendChatMessage(true, msg);
+            WakeLocker.release();
+        }
+    };
+}}
